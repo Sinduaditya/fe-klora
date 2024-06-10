299326ca2f320b35163c9d0b45ab44db05da4e44
SinduAditya <nduujanadi51@gmail.com>
2024-06-10 12:43:32 +0700

    first commit

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ import "./index.css";
 const manifestUrl =
   "https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json";
 
-// WebApp.ready();
+WebApp.ready();
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
     <WalletProvider>
